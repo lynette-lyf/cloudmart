c490d7e2853bd05f836e4bbb718c22e5be2b3499
Ubuntu <ubuntu@ip-172-31-81-205.ec2.internal>
2019-11-20 05:35:31 +0000

    Added Origin FK

--- shop/models.py
+++ shop/models.py
@@ -3,9 +3,11 @@ from django.db import models
 #NOTE TO SELF: make migrations everytime model is changed
 
 # Create your models here.
+
 class Product(models.Model):
     name = models.CharField(max_length=255, blank=False)
     sku = models.CharField(max_length=100, blank=False)
+    origin = models.ForeignKey('Origin', on_delete=models.CASCADE)
     description = models.TextField(blank=False)
     cost = models.IntegerField(blank=False)
     quantity = models.IntegerField(blank=False, default=0)
@@ -17,3 +19,9 @@ class Product(models.Model):
     
     def getCostInDollars(self):
         return self.cost/100
+
+class Origin(models.Model):
+    name = models.CharField(max_length=255, blank=False)
+    
+    def __str__(self):
+        return self.name
