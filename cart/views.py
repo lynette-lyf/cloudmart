5585bc5fbf2ca928d28a2669d2613bc0587d1f03
Ubuntu <ubuntu@ip-172-31-81-205.ec2.internal>
2019-11-30 15:14:59 +0000

    Able to view total cost of each product and total cost of all products in the cart

--- cart/views.py
+++ cart/views.py
@@ -3,12 +3,37 @@ from .models import CartItem
 from shop.models import Product
 
 # Create your views here.
+def calculate_product_cost(request):
+    existing_cart_item = CartItem.objects.filter(owner=request.user)
+    product_cost = 0
+    for cart_item in existing_cart_item:
+        product_cost += cart_item.product.cost * cart_item.quantity
+    
+    return product_cost
+    
+# Calculate total cost in cart
+def calculate_cart_cost(request):
+    all_cart_items = CartItem.objects.filter(owner=request.user)
+    amount = 0
+    for cart_item in all_cart_items:
+        amount += cart_item.product.cost * cart_item.quantity
+    
+    return amount
+    
 def view_cart(request):
     # request.user = user that is currently logged in
     all_cart_items = CartItem.objects.filter(owner=request.user)
+    # total_cost = calculate total cost in cart
+    total_cost = calculate_cart_cost(request)
+    
+    product_cost = calculate_product_cost(request)
+    
     return render(request, 'cart/view_cart.template.html', {
-        'all_cart_items': all_cart_items
+        'all_cart_items': all_cart_items,
+        'product_cost': product_cost/100,
+        'total_cost': total_cost/100
     })
+
     
 def add_to_cart(request, product_id):
     
@@ -36,3 +61,6 @@ def remove_from_cart(request, cart_item_id):
     existing_cart_item = CartItem.objects.get(pk=cart_item_id)
     existing_cart_item.delete()
     return redirect(reverse('catalog'))
+
+
+
--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from .views import checkout, charge
 
 urlpatterns = [
-    path('', checkout, name='checkout'),
     path('charge/', charge, name='charge')
 ]
