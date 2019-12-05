3d9b10158c665e6d2d8fa8ff7f14eca31d0fb220
Ubuntu <ubuntu@ip-172-31-81-205.ec2.internal>
2019-12-05 08:31:48 +0000

    Convert database from sqlite3 to Postgres and passed environment variables into Heroku

--- CloudMart/settings.py
+++ CloudMart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,12 +82,12 @@ WSGI_APPLICATION = 'CloudMart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
@@ -176,3 +177,5 @@ DEFAULT_FILE_STORAGE='custom_storages.MediaStorage'
 
 MESSAGE_STORAGE='django.contrib.messages.storage.session.SessionStorage'
 
+
+DATABASES = {'default': dj_database_url.parse(os.environ["DATABASE_URL"])}
